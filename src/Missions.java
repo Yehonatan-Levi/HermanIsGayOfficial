9d6b5c7c21425f03f1fc3c9f2d4e4ce86f8414aa
Yehonatan-Levi <yehonatan.levi0904@gmail.com>
2024-11-29 18:16:47 +0200

    name: Yehonatan Levi info: Herman is an ooble booble

--- src/Missions.java
+++ src/Missions.java
@@ -2,4 +2,17 @@ public class Missions {
     public static void ex1(double n1, double n2, double n3){
         System.out.println((n1 + n2 + n3) / 3);
     }
+
+    public static void ex2(double n1, double n2){
+        if (n1 % 2 == 0 && n2 % 2 == 0) {
+            System.out.println("Both numbers are even");
+        } else {
+            System.out.println("Womp Womp not bouth of the numbers are even");
+        }
+        if ((n1 + n2) / 2 > n1){
+            System.out.println("No way!!! the average is bigger then Herman's bana-sorry i meant the first number");
+        } else {
+            System.out.println("The average is smaller then the second number");
+        }
+    }
 }
--- src/Tester.java
+++ src/Tester.java
@@ -1,5 +1,6 @@
 public class Tester {
     public static void main(String[] args){
         Missions.ex1(12, 15.2, 9.1);
+        Missions.ex2(18, 44);
     }
 }
