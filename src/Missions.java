d89f91130e0dbef19f335e4c959e277be35a3e5c
Yehonatan-Levi <yehonatan.levi0904@gmail.com>
2024-11-30 19:26:30 +0200

    Yehonatan Levi: _/0o0\_

--- src/Missions.java
+++ src/Missions.java
@@ -1,3 +1,22 @@
 public class Missions {
+    public static void ex1() {
+        String[] array = new String[5];
+        array[0] = "Herman";
+        array[1] = "is";
+        array[2] = "super";
+        array[3] = "mega";
+        array[4] = "gay";
 
+        String output = "";
+        for (String s : array) {
+            output += s;
+        }
+        System.out.println(output);
+
+        output = "";
+        for (int i = 4; i >= 0; i--) {
+            output += array[i];
+        }
+        System.out.println(output);
+    }
 }
