a2f1bf5b68609ac8b227c0c81a4dfd71fe3521f7
greenblitz4590 <94316058+greenblitz4590@users.noreply.github.com>
2024-12-04 18:09:41 +0200

    Levi: Mission is complete:)

--- src/Missions.java
+++ src/Missions.java
@@ -1,11 +1,22 @@
 public class Missions {
 
     public interface StringManipulation {
-        String manipulate(String stringManipulation);
+        String manipulate(String text);
     }
 
-    public static StringManipulation toUpperCase = input -> input.toUpperCase();
-    StringManipulation toLowerCase = input -> input.toLowerCase();
+    StringManipulation toUpperCase = text -> text.toUpperCase();
+    StringManipulation toLowerCase = text -> text.toLowerCase();
 
+    public static void exStringManipulation(String text) {
+        System.out.println(text);
+        System.out.println(new Missions().toLowerCase.manipulate(text));
+        System.out.println(new Missions().toUpperCase.manipulate(text));
+    }
+
+    public interface CheckString {
+        char[] check(String text1, String text2);
+    }
+
+    CheckString whoSmaller = (text1, text2) -> {return (text1.length() < text2.length()) ? text1.toCharArray(): text2.toCharArray();};
 
 }
--- src/Tester.java
+++ src/Tester.java
@@ -2,8 +2,8 @@ public class Tester {
 
     public static void main(String[] args){
         String name = "HermAN";
-        name = new Missions().toLowerCase.manipulate(name);
-        System.out.println(name);
+        Missions.exStringManipulation(name);
+
 
     }
 }
